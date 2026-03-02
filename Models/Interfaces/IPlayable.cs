ac0519bee8ee4c65e6e32be5ae487a11e2a82dfa
x341dev <admin@x341.dev>
2026-03-02 13:35:39 +0100

    feat: add methods to IEatable, IPlayable, and ISleepable interfaces
    closes #19

--- Models/Interfaces/IPlayable.cs
+++ Models/Interfaces/IPlayable.cs
@@ -2,5 +2,5 @@ namespace XBochi.Models.Interfaces;
 
 public interface IPlayable
 {
-    
+    void Play();
 }
--- Models/Interfaces/ISleepable.cs
+++ Models/Interfaces/ISleepable.cs
@@ -2,5 +2,5 @@ namespace XBochi.Models.Interfaces;
 
 public interface ISleepable
 {
-    
+    void Sleep();
 }
