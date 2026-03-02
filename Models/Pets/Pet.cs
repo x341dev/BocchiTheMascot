389962f9a8b7fac6af13c4e4973e82a30baa8758
x341dev <admin@x341.dev>
2026-03-02 12:04:42 +0100

    feat: add properties and methods for Pet class
    closes #11

--- Models/Pets/Pet.cs
+++ Models/Pets/Pet.cs
@@ -3,6 +3,10 @@ using XBochi.Models.Enums;
 namespace XBochi.Models.Pets;
 
 public abstract class Pet
-{  
-
+{
+    protected string Name { get; set; }
+    protected Emotions Emotion { get; set; }
+    
+    public string GetName() => Name;
+    public Emotions GetEmotion() => Emotion;
 }
