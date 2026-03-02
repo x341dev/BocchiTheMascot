67736b6e2a54ab819291278b136d75134c6aa109
x341dev <admin@x341.dev>
2026-03-02 12:26:52 +0100

    feat: create Stats for the mascot
    closes #13

--- Models/Core/Stats.cs
+++ Models/Core/Stats.cs
@@ -2,5 +2,9 @@ namespace XBochi.Models.Core;
 
 public class Stats
 {
-    
+    public int Hunger { get; set; } = 100;
+    public int Energy { get; set; } = 100;
+    public int SnackCount { get; set; } = 0;
+
+    public int Health => (Hunger + Energy) / 2;
 }
