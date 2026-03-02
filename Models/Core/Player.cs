a6261f8c712325093da48f1e9c61e227c2eefd50
x341dev <admin@x341.dev>
2026-03-02 13:15:55 +0100

    feat: implement Player class with pet selection and inventory management
    closes #16, closes #17

--- Models/Core/Player.cs
+++ Models/Core/Player.cs
@@ -1,6 +1,37 @@
+using XBochi.Models.Pets;
+using XBochi.UI;
+
 namespace XBochi.Models.Core;
 
 public class Player
 {
+    public string Name { get; private set; }
+    public Pet CurrentPet { get; set; }
+    public Inventory PlayerInventory { get; private set; }
+
+    public Player(string name)
+    {
+        Name = name;
+        PlayerInventory = new Inventory(10);
+    }
+
+    public void ChoosePet()
+    {
+        int petType = GameUI.SelectPet();
+        string petName = GameUI.GetPetName();
+        switch (petType)
+        {
+            case 1:
+                CurrentPet = new Chick(petName);
+                break;
+            case 2:
+                CurrentPet = new Dog(petName);
+                break;
+            case 3:
+                CurrentPet = new Cat(petName);
+                break;
+        }
+        
+    }
     
 }
