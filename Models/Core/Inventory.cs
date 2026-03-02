958ff8f9f5da9167b20ee36ad105c0f124a4f83f
x341dev <admin@x341.dev>
2026-03-02 13:29:25 +0100

    feat: add Inventory class
    closes #22

--- Models/Core/Inventory.cs
+++ Models/Core/Inventory.cs
@@ -1,6 +1,15 @@
+using XBochi.Models.Items;
+
 namespace XBochi.Models.Core;
 
 public class Inventory
 {
+    private Item[] _items;
+
+    public Inventory(int capacity)
+    {
+        _items = new Item[capacity];
+    }
+    
     
 }
